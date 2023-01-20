8a3270563a9ef03b2901d3670537b606ddbc8b82
floppyfloppa <nova.lindberg@elev.ga.ntig.se>
2023-01-20 11:02:32 +0100

    fortsattfortsatte med min del

--- src/novadfiler.java
+++ src/novadfiler.java
@@ -1,6 +1,11 @@
+import java.io.*;
 public class novadfiler {
     public static void main(String[] args) {
-        String[] ord = {"röd"};
+        String[] ordlista = {"röd","blå","gul","orange","lila","grön"};
+        String ord = "slumpat ord";
+        = ord.length();
+        System.out.println(ord.length());
+
 
     }
-
+}
